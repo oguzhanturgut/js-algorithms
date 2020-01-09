df657c94e9ffa1b7940797a3b375843d9e0d64a3
Oguzhan <oguzhanturgut@outlook.com>
2020-01-09 19:58:44 +0100

    solve reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {
+  return [...str].reduce((a, c) => c + a, '');
+}
 module.exports = reverse;
+
+// Solution 1
+// function reverse(str) {
+//   return [...str].reverse().join('');
+// }
+
+// Solution 2
+// function reverse(str) {
+//   let res = '';
+//   for (let i = 0; i < str.length; i++) {
+//     res = str[i] + res;
+//   }
+//   return res;
+// }
+
+// Solution 3
+// function reverse(str) {
+//   let res = '';
+//   for (let char of str) {
+//     res = char + res;
+//   }
+//   return res;
+// }
