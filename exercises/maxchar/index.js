25aab678b7d09f8b8a6d7483f16230847d906692
Oguzhan <oguzhanturgut@outlook.com>
2020-01-11 02:07:58 +0100

    solve maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const strObj = [...str].reduce((a, c) => {
+    a[c] = a[c] + 1 || 1;
+    return a;
+  }, {});
+
+  let max = 0;
+  let res = '';
+  for (let key in strObj) {
+    if (strObj[key] > max) {
+      max = strObj[key];
+      res = key;
+    }
+  }
+  return res;
+}
 
 module.exports = maxChar;
