eeedc542b6aed5965fc9acc5150af4d6f620fcab
Oguzhan <oguzhanturgut@outlook.com>
2020-03-25 23:30:38 +0100

    fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,30 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Recursive with memoization
+const memo = fn => {
+  const store = {};
+  return n => {
+    if (store[n]) return store[n];
+    store[n] = fn(n);
+    return store[n];
+  };
+};
+
+const recursiveFib = n => {
+  if (n === 0 || n === 1) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const fib = memo(recursiveFib);
+
+
+// const fib = n => {
+//   const arr = [0,1];
+//   for (let i = 1; i < n+1; i++) {
+//     arr[i + 1] = arr[i - 1] + arr[i];
+//   }
+//   return arr[n];
+// };
 
 module.exports = fib;
