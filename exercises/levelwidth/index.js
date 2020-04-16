7c8355155dde79efcd1ebcefd778a5b59209b460
Oguzhan <oguzhanturgut@outlook.com>
2020-04-16 14:48:39 +0200

    levelwidth solution with BF algo

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const counters = [0];
+  const queue = [root, null];
+
+  while (queue.length > 1) {
+    const nextNode = queue.shift();
+    if (nextNode === null) {
+      counters.push(0);
+      queue.push(null);
+    } else {
+      queue.push(...nextNode.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+
+}
 
 module.exports = levelWidth;
